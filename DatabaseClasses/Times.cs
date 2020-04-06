b49e76473dc6416288b60264e5db320803c8611e
AndreasAskjem <andaskjem@gmail.com>
2020-04-06 10:32:53 +0200

    a

--- DatabaseClasses/Times.cs
+++ DatabaseClasses/Times.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace RubiksTider.Model
+namespace RubiksTider.DatabaseClasses
 {
-    public class Data
+    public partial class Times
     {
         public int Id { get; set; }
         public double Time { get; set; }
